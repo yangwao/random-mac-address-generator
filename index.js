4a5a357cbc9fbfa106b87ea2b9f41ff4399367a7
Matej Nemcek <ybdaba@gmail.com>
2017-09-03 14:20:17 +0200

    naah, im ok

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const { execSync } = require('child_process');
-
 const fs = require('fs')
 const mac = fs.readFileSync('oui-mac.txt', 'utf8')
 const macArray = mac.split("\n")
 let validVendorMacs = []
 
-for ( i in macArray) {
+for (let i in macArray) {
   validVendorMacs.push(macArray[i].split(' ')[0].toLowerCase().split(""))
 }
 
-const randomValidVendorMac = validVendorMacs[Math.floor((Math.random() * validVendorMacs.length) +1)]
+const randomValidVendorMac =
+  validVendorMacs[Math.floor((Math.random()
+  * validVendorMacs.length) + 1)]
 
 let userMac = [...Array(6)]
-
 for (let i = 0; i < userMac.length; i++) {
-  userMac[i] = (Math.floor(Math.random()*16).toString(16));
+  userMac[i] = (Math.floor(Math.random() * 16).toString(16));
 }
 
-const finalMacArray = [...randomValidVendorMac,...userMac]
-
-const finalMacAddress = finalMacArray.map((n, i) => i % 2 ? n + ':' : n).join('').slice(0, -1);
+const finalMacArray = [...randomValidVendorMac, ...userMac]
+const finalMacAddress =
+  finalMacArray.map((n, i) => i % 2 ? n + ':' : n).join('').slice(0, -1);
 
 const { spawn } = require('child_process')
 const ifconfig = spawn('ifconfig', ['en0'])
@@ -42,9 +41,18 @@ ifconfig.on('close', (code) => {
 })
 
 grep.stdout.on('data', (data) => {
-      realMac = data.toString().split(" ")
-      console.log(realMac[1]);
-      })
+  realMac = data.toString().split(" ")
+  console.log(`
+    write down your real(own) mac address
+    ⫸ ifconfig en0 | grep ether
+    => your current mac address is ${realMac[1]}
+    Set new random mac with real vendor
+    ⫸ sudo ifconfig en0 ether ${finalMacAddress}
+    Turn Off & On wifi
+    ⫸ networksetup -setairportpower en0 off
+    ⫸ networksetup -setairportpower en0 on
+  `)
+})
 
 grep.stderr.on('data', (data) => {
   console.log(`grep stderr: ${data}`)
@@ -55,17 +63,6 @@ grep.on('close', (code) => {
     console.log(`grep process exited with code ${code}`)
   }
 })
-
-
-console.log(`
-  write down your real-own mac address. Run
-  ⫸ ifconfig en0 | grep ether
-  Set new random mac *with real vendor*
-  ⫸ sudo ifconfig en0 ether ${finalMacAddress}
-  turn off & on wifi
-  ⫸ networksetup -setairportpower en0 off
-  ⫸ networksetup -setairportpower en0 on
-`);
 
 // let en0down = execSync('networksetup -setairportpower en0 off')
 // let en0up = execSync('networksetup -setairportpower en0 on')
